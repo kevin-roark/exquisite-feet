9e7faddc72ec6a1e0d064892df008cbf510900df
Kevin Roark <ker2143@columbia.edu>
2014-10-22 01:28:13 -0400

    got them flashers'

--- feet.js
+++ feet.js
@@ -2,13 +2,62 @@ $(function() {
   var TOTAL_HEIGHT = 760;
   var TOTAL_WIDTH = 600;
 
-  var VERTICAL_SPACE = TOTAL_HEIGHT / 4;
+  var VERTICAL_SPACE = TOTAL_HEIGHT / 4; // 190
   var START_Y = VERTICAL_SPACE * 3;
   var END_Y = TOTAL_HEIGHT;
+
+  var imageNames = [
+    'boots.jpg',
+    'candy.jpg',
+    'cow.jpg',
+    'dog.jpg',
+    'female.jpg',
+    'flops.jpg',
+    'horse.jpg',
+    'kimodo.jpg',
+    'kiss.jpg',
+    'machine.jpg',
+    'male.jpg',
+    'military.jpg',
+    'penguin.jpg',
+    'pig.jpg',
+    'robot.png',
+    'tapdance.jpg',
+    'therapy.jpg'
+  ];
+
+  for (var i = 0; i < imageNames.length; i++) {
+    var source = 'images/' + imageNames[i];
+
+    imageNames[i] = new Image(TOTAL_WIDTH, VERTICAL_SPACE);
+    imageNames[i].src = source;
+  }
 
   var canvas = document.querySelector('#corpse');
   var context = canvas.getContext('2d');
 
-  context.fillStyle = 'red';
-  context.fillRect(0, START_Y, TOTAL_WIDTH, END_Y);
+  flashFeet();
+
+  function flashFeet() {
+    clearFootZone();
+    drawRandomImageInFootZone();
+
+    var minTime = 50;
+    var maxTime = 250;
+    var timeout = Math.floor(Math.random() * (maxTime - minTime)) + minTime;
+    setTimeout(flashFeet, timeout);
+  }
+
+  function clearFootZone() {
+    context.clearRect(0, START_Y, TOTAL_WIDTH, END_Y);
+  }
+
+  function drawRandomImageInFootZone() {
+    var image = choice(imageNames);
+    context.drawImage(image, 0, START_Y, TOTAL_WIDTH, VERTICAL_SPACE);
+  }
+
+  function choice(items) {
+    return items[Math.floor(Math.random()*items.length)];
+  }
 });
